8efe85e593a7e3969c91ccff172a7aca16adddd3
diegoahenao <ggodie85@gmail.com>
2024-06-12 01:44:30 -0500

    feat(endpoints): Creación de endpoints y seguridad

---
+++ app/auth/security.py
@@ -0,0 +1,59 @@
+import os
+from datetime import datetime, timedelta, timezone
+from fastapi import Depends, HTTPException, Security
+from fastapi.security.api_key import APIKeyHeader
+from fastapi.security.oauth2 import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from pydantic import BaseModel
+from typing import Optional
+
+API_KEY_NAME = os.environ.get('API_KEY_NAME')
+API_KEY = os.environ.get('API_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+ALGORITHM = os.environ.get('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES'))
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def get_api_key(api_key_header: str = Security(api_key_header)):
+    if api_key_header == API_KEY:
+        return api_key_header
+    else:
+        raise HTTPException(
+            status_code=403, detail="Could not validate credentials"
+        )
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    return token_data
--- app/main.py
+++ app/main.py
@@ -2,10 +2,16 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 import app.models.table_models as model
 from app.database.database import engine
+from app.routers.table_routers import tables_router
+from app.routers.auth_routers import auth_router
 
 app = FastAPI()
 app.title = "Mercado Libre Code Challenge"
 app.version = "0.0.1"
+
+# Routers
+app.include_router(tables_router)
+app.include_router(auth_router)
 
 # Crear tablas
 model.Base.metadata.create_all(bind = engine)
